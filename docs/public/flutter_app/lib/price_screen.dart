d56e5f0f1957c0066ea8f7c2f0cf72a6971884a2
Lori Framework Team <frameworklori@gmail.com>
2025-07-02 10:57:42 +0900

    Update price_screen.dart

--- docs/public/flutter_app/lib/price_screen.dart
+++ docs/public/flutter_app/lib/price_screen.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatelessWidget {
+  final Map<String, double> prices = {
+    'USA': 300,
+    'Japan': 30,
+    'India': 10,
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Drug Price Comparison')),
+      body: ListView(
+        children: prices.entries.map((entry) {
+          return ListTile(
+            title: Text(entry.key),
+            trailing: Text('\$${entry.value.toStringAsFixed(2)}'),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
 
